f42b5a0b6dd873926a71616f7b70e6bff2ed334a
Leonardo Araújo Freitas <62023102+Leonardo1924@users.noreply.github.com>
2022-02-28 15:36:43 +0000

    Adding Initial Struct para o Projeto

---
+++ ENGINE/main.cpp
@@ -0,0 +1,107 @@
+#ifdef __linux__
+#include <unistd.h>
+#elif _WIN32
+#include <direct.h>
+#include <io.h>
+#else
+#endif
+
+#include <GL/glut.h>
+#include <math.h>
+
+//Variables for keyboard fuction
+float r = 10.0f;
+float alpha = 0.5;
+float beta = 0.5;
+
+/**
+ * Function that redimensionates a window.
+ * @param w width of the window.
+ * @param h height of the window.
+ */
+void changeSize(int w, int h) {
+
+    // Prevent a divide by zero, when window is too short
+    // (you cant make a window with zero width).
+    if (h == 0)
+        h = 1;
+
+    // compute window's aspect ratio
+    float ratio = w * 1.0 / h;
+
+    // Set the projection matrix as current
+    glMatrixMode(GL_PROJECTION);
+    // Load Identity Matrix
+    glLoadIdentity();
+
+    // Set the viewport to be the entire window
+    glViewport(0, 0, w, h);
+
+    // Set perspective
+    gluPerspective(45.0f, ratio, 1.0f, 1000.0f);
+
+    // return to the model view matrix mode
+    glMatrixMode(GL_MODELVIEW);
+}
+
+/**
+ * Function that creates a scene.
+ */
+void renderScene(void) {
+
+    // clear buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // set the camera
+    glLoadIdentity();
+
+    gluLookAt(sin(beta) * cos(alpha) * r, sin(alpha) * r, cos(beta) * cos(alpha) * r, //camera position
+        0.0, 0.0, 0.0,  // where to look
+        0.0f, 1.0f, 0.0f); //up vector
+
+    //AXIS
+    glBegin(GL_LINES);
+    // X axis in red
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex3f(-100.0f, 0.0f, 0.0f);
+    glVertex3f(100.0f, 0.0f, 0.0f);
+    // Y Axis in green
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(0.0f, -100.0f, 0.0f);
+    glVertex3f(0.0f, 100.0f, 0.0f);
+    // Z Axis in blue
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glVertex3f(0.0f, 0.0f, -100.0f);
+    glVertex3f(0.0f, 0.0f, 100.0f);
+    glEnd();
+
+    //DRAW Instruction Here
+    
+
+    // End of frame
+    glutSwapBuffers();
+}
+
+bool initGlut(int argc, char** argv){
+    
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(800, 800);
+    glutCreateWindow("ProjetoCG");
+
+    // Required callback registry
+    glutDisplayFunc(renderScene);
+    glutReshapeFunc(changeSize);
+
+    // put here the registration of the keyboard callbacks
+    
+
+    //  OpenGL settings
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+
+    // enter GLUT's main cycle
+    glutMainLoop();
+    return true;
+}
---
+++ Gerador/main.cpp
@@ -0,0 +1,8 @@
+#ifdef __linux__
+#include <unistd.h>
+#elif _WIN32
+#include <direct.h>
+#include <io.h>
+#else
+#endif
+
