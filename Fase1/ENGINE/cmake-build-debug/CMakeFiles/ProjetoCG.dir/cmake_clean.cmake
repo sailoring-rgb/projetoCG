8ee703fa761300c3d81427f363b00170a9e00b84
Ana Henriques <pbapohenriques@hotmail.com>
2022-06-03 11:10:23 +0100

    textures plane + box

---
+++ Fase1/ENGINE/cmake-build-debug/CMakeFiles/ProjetoCG.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjetoCG.dir/Point.cpp.o"
+  "CMakeFiles/ProjetoCG.dir/Point.cpp.o.d"
+  "CMakeFiles/ProjetoCG.dir/Primitive.cpp.o"
+  "CMakeFiles/ProjetoCG.dir/Primitive.cpp.o.d"
+  "CMakeFiles/ProjetoCG.dir/main.cpp.o"
+  "CMakeFiles/ProjetoCG.dir/main.cpp.o.d"
+  "CMakeFiles/ProjetoCG.dir/tinyxml2.cpp.o"
+  "CMakeFiles/ProjetoCG.dir/tinyxml2.cpp.o.d"
+  "ProjetoCG"
+  "ProjetoCG.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjetoCG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Fase4/Generator/cmake-build-debug/CMakeFiles/GENERATOR.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/annaphens/Desktop/college/CG/projetoCG/Fase4/Generator/generator.cpp" "CMakeFiles/GENERATOR.dir/generator.cpp.o" "gcc" "CMakeFiles/GENERATOR.dir/generator.cpp.o.d"
+  "/Users/annaphens/Desktop/college/CG/projetoCG/Fase4/Generator/tinyxml2.cpp" "CMakeFiles/GENERATOR.dir/tinyxml2.cpp.o" "gcc" "CMakeFiles/GENERATOR.dir/tinyxml2.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
